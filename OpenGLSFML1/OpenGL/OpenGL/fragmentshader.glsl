60e20fdd20e5d6cec3d4a98ea99caa0929f12ec6
omarsamy3 <omar.samy3333@gmail.com>
2022-04-08 13:43:02 +0200

    This is the fragment shader file with various editions

---
+++ OpenGLSFML1/OpenGL/OpenGL/fragmentshader.glsl
@@ -0,0 +1,15 @@
+#version 330 core
+
+out vec4 FragColor;
+in vec3 colorout;
+in vec2 TexCoord;
+
+uniform vec4 coloru;
+
+uniform sampler2D texture1;
+
+
+void main()
+{
+	FragColor = vec4(colorout , 1);	
+}
